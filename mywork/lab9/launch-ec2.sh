beed76238e7fae84a2644e3c776f83e7769eb2f1
Shlok Rawal <sub5gd@virginia.edu>
2026-04-01 18:58:10 -0400

    Completed Lab 09 EC2 provisioning

---
+++ mywork/lab9/launch-ec2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+AMI="ami-0ec10929233384c7f"             
+INSTANCE_TYPE="t2.nano"                 
+INSTANCE_NAME="ds2002-sub5gd"  
+KEY_NAME="key-ec2"                      
+SECURITY_GROUP_ID="sg-0be6679b34372f15a" 
+SUBNET_ID="subnet-085a38474f2e3b60c"     
+
+
+echo "Launching EC2 instance: $INSTANCE_NAME..."
+
+aws ec2 run-instances \
+  --image-id $AMI \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --security-group-ids $SECURITY_GROUP_ID \
+  --subnet-id $SUBNET_ID \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]"
+
+echo "Launch request sent!"
